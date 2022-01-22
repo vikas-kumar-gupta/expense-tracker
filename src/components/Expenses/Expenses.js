09aa7cef1e4b6ce95161169697210d91cdf52e78
Vikas Kumar Gupta <vikas.vg118@gmail.com>
2022-01-22 14:09:04 +0530

    initial setup

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./Expenses.css";
+
+import Card from '../UI/Card';
+
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        date={props.item[0].date}
+        amount={props.item[0].amount}
+      />
+      <ExpenseItem
+        title={props.item[1].title}
+        date={props.item[1].date}
+        amount={props.item[1].amount}
+      />
+      <ExpenseItem
+        title={props.item[2].title}
+        date={props.item[2].date}
+        amount={props.item[2].amount}
+      />
+      <ExpenseItem
+        title={props.item[3].title}
+        date={props.item[3].date}
+        amount={props.item[3].amount}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
