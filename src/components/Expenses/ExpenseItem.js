24ad64b6dfd24aa6c1a36748c51296e78a095278
Vikas Kumar Gupta <vikas.vg118@gmail.com>
2022-01-23 14:52:56 +0530

    extra button removed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 
@@ -6,21 +6,12 @@ import Card from "../UI/Card"
 import ExpenseDate from "./ExpenseDate.js";
 
 const ExpenseItem = (props) => {
-
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated');
-    console.log(title);
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
-        <button onClick={clickHandler}>Click me</button>
       </div>
     </Card>
   );
